619e6527f632c5a1ad764b4c96e4fc0d01209f0f
himasada <bkesav3@gmail.com>
2019-05-28 11:35:02 -0700

    first commit

---
+++ src/test/java/MavenTest/MavenTestP/MavpracTest.java
@@ -0,0 +1,21 @@
+package MavenTest.MavenTestP;
+
+import org.testng.annotations.Test;
+
+public class MavpracTest {
+	@Test
+	public void sum()
+	{
+		int a= 2;
+		int b= 3;
+		System.out.println(a+b);
+	}
+	@Test
+	public void sub()
+	{
+		int a= 2;
+		int b= 3;
+		System.out.println(b-a);
+	}
+
+}
